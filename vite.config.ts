eacc4070d901f06ee34a5f64e6c70ae4fab66b39
Aayan Shah <ashah28@colby.edu>
2025-10-16 18:05:54 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/resume-folio-bloom/",
+  base: "",
   build: {
     outDir: "out",
   },
