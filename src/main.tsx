4887ef067d674696f02c884cd058628a0a264d6d
aayans314 <ashah28@colby.edu>
2025-10-16 18:33:12 -0400

    Deploy failed, save point before next disaster

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,5 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(  <BrowserRouter basename="/resume-folio-bloom">
-    <App />
-  </BrowserRouter>);
+createRoot(document.getElementById("root")!).render(
+    <App />);
